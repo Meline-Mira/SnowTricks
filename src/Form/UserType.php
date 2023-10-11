3bc5c417bfb84ce48370466385acb239c27a47f5
Emeline Gineys <emeline.gineys@gmail.com>
2023-10-11 17:45:22 +0200

    issue 11 : form changes

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,10 +16,10 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('password', TextType::class)
-            ->add('username', TextType::class)
-            ->add('picture', TextType::class)
+            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
+            ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
+            ->add('picture', TextType::class, ['label' => 'Votre photo'])
             ->add('Enregistrer', SubmitType::class)
         ;
     }
